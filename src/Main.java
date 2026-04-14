5fdbb7c9bc9d020635fca1840bd5c70175855a0e
wwoosshh <s01055310644@gmail.com>
2026-04-14 16:37:14 +0900

    week2-1: 구구단 + 재시도루프(do-while)

--- src/Main.java
+++ src/Main.java
@@ -3,31 +3,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        System.out.print("첫 번째 숫자: ");
-        double a = sc.nextDouble();
-
-        System.out.print("두 번째 숫자: ");
-        double b = sc.nextDouble();
-
-        sc.nextLine();
-        System.out.print("연산자 (+,-,*,/): ");
-        String op = sc.nextLine();
-
-        if (op.equals("+")) {
-            System.out.println("결과: " + (a + b));
-        } else if (op.equals("-")) {
-            System.out.println("결과: " + (a - b));
-        } else if (op.equals("*")) {
-            System.out.println("결과: " + (a * b));
-        } else if (op.equals("/")) {
-            if (b == 0) {
-                System.out.println("에러: 0으로 나눌 수 없습니다.");
-            } else {
-                System.out.println("결과: " + (a / b));
+        int dan;
+        do {
+            System.out.print("몇 단? ");
+            dan = sc.nextInt();
+            if (dan < 2 || dan > 9) {
+                System.out.println("2~9 사이의 숫자를 입력해주세요.");
             }
-        } else {
-            System.out.println("알 수 없는 연산자");
+        } while (dan < 2 || dan > 9);
+
+        for (int i = 1; i <= 9; i++) {
+            System.out.printf("%d x %d = %d%n", dan, i, dan * i);
         }
+        sc.close();
     }
 }
